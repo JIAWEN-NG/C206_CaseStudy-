f747fbbce31690a7e7b033efaf51b36cf5df09f9
20011454 <20011454@LAPTOP-LUNVOJS5>
2021-07-28 20:36:16 +0800

    Testing

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,10 +3,10 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//testing
-		//new testing
 		
 		//jia wen 
+		
+		// Jia Wen always sleep in class
 	}
 
 }
